a86313809741b9554c1323f3fb11c2d090515d81
code-rishabh <r08s2001@gmail.com>
2025-03-13 11:42:12 +0530

    Refactor: Migrate Next.js configuration from JavaScript to TypeScript, enhancing image remote patterns and experimental server actions settings. Remove outdated JavaScript config file.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'tasl.runtimetheory.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tasl.runtimetheory.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      allowedOrigins: ['*']
+    }
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  }
+}
 
 export default nextConfig;
